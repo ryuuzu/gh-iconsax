5c164172b28db1304c2ca7418ac8129c59b09128
Utsav Gurmachhan - Main <utsavmagar88728@gmail.com>
2022-11-28 18:59:58 +0545

    initial react setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./isaxstyle.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					<i className="isax isax-align-vertically"></i>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Isax.js
@@ -0,0 +1,5 @@
+function IsaxIcon() {
+	return <div></div>;
+}
+
+export default IsaxIcon;
