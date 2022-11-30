1cdb79652000468aee0c67c022e7b4b21fd5495e
Utsav Gurmachhan - Main <utsavmagar88728@gmail.com>
2022-11-30 17:40:52 +0545

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.66f82974.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgh_iconsax=self.webpackChunkgh_iconsax||[]).push([[5539],{2689:function(a){function e(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=e,e.displayName="brainfuck",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.66f82974.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.8d1e5544.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgh_iconsax=self.webpackChunkgh_iconsax||[]).push([[3140],{7761:function(a){function s(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=s,s.displayName="hsts",s.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.8d1e5544.chunk.js.map
